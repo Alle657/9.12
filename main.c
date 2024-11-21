e05cfe060d31dbda31900e43a0cc3ca833163b09
Alle657 <alessandro.tarasco23@gmail.com>
2024-11-21 18:14:30 +0100

    9.12

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 int calcolaMCD(int num1, int num2) {
     while (num2 != 0) {
-        int temp = num2;
+        int variabileTemporanea = num2;
         num2 = num1 % num2;
-        num1 = temp;
+        num1 = variabileTemporanea;
     }
     return num1;
 }
