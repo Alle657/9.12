106888ae34379288a072eda67849a05d91bfc1dd
Alle657 <alessandro.tarasco23@gmail.com>
2024-11-21 18:10:17 +0100

    9.12

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int calcolaMCD(int num1, int num2) {
+    while (num2 != 0) {
+        int temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+    return num1;
+}
+
+
+int calcolaMCM(int num1, int num2) {
+    return (num1 / calcolaMCD(num1, num2)) * num2;
+}
+
+int main() {
+    int Inputnum1, Inputnum2;
+
+
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &Inputnum1);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &Inputnum2);
+
+
+    int mcm = calcolaMCM(Inputnum1, Inputnum2);
+
+
+    printf("Il minimo comune multiplo di %d e %d e ': %d\n", Inputnum1, Inputnum2, mcm);
+
+    return 0;
+}
